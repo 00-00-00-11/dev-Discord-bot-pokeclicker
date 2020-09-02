2fe883e8b59c589dc403693b42852250941b93f5
RedSparr0w <admin@redsparr0w.com>
2020-09-02 20:24:44 +1200

    Update display

--- commands/claim.js
+++ commands/claim.js
@@ -1,6 +1,8 @@
+const { MessageEmbed } = require('discord.js');
 const { getDB, getUserID, addAmount } = require('../database.js');
 
-const MINUTE = 1000 * 60;
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
@@ -14,6 +16,7 @@ const getLastClaimTime = async (user) => {
   ]);
 
   const { last_claim } = await db.get('SELECT last_claim FROM coins WHERE user=?', user_id) || { last_claim: '0' };
+  db.close();
   return new Date(last_claim);
 };
 
@@ -32,11 +35,12 @@ const setLastClaimTime = async (user) => {
   };
 
   await db.run('UPDATE coins SET last_claim=$date WHERE user=$user_id', data);
+  db.close();
 };
 
 module.exports = {
   name        : 'claim',
-  aliases     : ['timely'],
+  aliases     : ['daily', 'timely'],
   description : 'Short description',
   args        : [],
   guildOnly   : true,
@@ -47,13 +51,24 @@ module.exports = {
     // Check if user claimed within the last 24 hours
     const lastClaim = await getLastClaimTime(msg.author);
     // TODO: display time left until next claim
-    if (lastClaim >= (Date.now() - DAY)) return msg.reply('You\'ve already claimed your coins for today..');
+    if (lastClaim >= (Date.now() - DAY)) {
+      const timeLeft = (+lastClaim + DAY) - Date.now();
+      const hours = Math.floor(timeLeft % DAY / HOUR).toString().padStart(2, '0');
+      const minutes = Math.floor(timeLeft % HOUR / MINUTE).toString().padStart(2, '0');
+      const seconds = Math.floor(timeLeft % MINUTE / SECOND).toString().padStart(2, '0');
+      const timeRemaining = `${hours} hours ${minutes} minutes ${seconds} seconds`;
+      return msg.channel.send({
+        embed: new MessageEmbed().setColor('#e74c3c').setDescription(`${msg.author}\nYou've already claimed your <:money:737206931759824918> for today\nYou can claim again in ${timeRemaining}`),
+      });
+    }
 
     // Add the coins to the users balance, set last claimed time
     const dailyAmount = 100;
     // Add the balance then set last claim time (incase the user doesn't exist yet)
     const balance = await addAmount(msg.author, dailyAmount, 'coins');
     await setLastClaimTime(msg.author);
-    return msg.reply(`Claimed ${dailyAmount.toLocaleString('en-US')} <:money:737206931759824918>,\nCurrent Balance: ${balance.toLocaleString('en-US')} <:money:737206931759824918>`);
+    return msg.channel.send({
+      embed: new MessageEmbed().setColor('#2ecc71').setDescription(`${msg.author}\n**+${dailyAmount.toLocaleString('en-US')}** <:money:737206931759824918>\n\nCurrent Balance: **${balance.toLocaleString('en-US')}** <:money:737206931759824918>`),
+    });
   },
 };
