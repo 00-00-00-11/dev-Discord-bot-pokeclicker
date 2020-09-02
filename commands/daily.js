eee1f35cc5d43b29cc20460519895eb4ac4cb0bf
RedSparr0w <admin@redsparr0w.com>
2020-09-02 18:54:29 +1200

    Add daily command

---
+++ commands/daily.js
@@ -0,0 +1,59 @@
+const { getDB, getUserID, addAmount } = require('../database.js');
+
+const MINUTE = 1000 * 60;
+const HOUR = 1000 * 60 * 60;
+const DAY = 1000 * 60 * 60 * 24;
+
+const getLastClaimTime = async (user) => {
+  const [
+    db,
+    user_id,
+  ] = await Promise.all([
+    getDB(),
+    getUserID(user),
+  ]);
+
+  const { last_claim } = await db.get('SELECT last_claim FROM coins WHERE user=?', user_id) || { last_claim: '0' };
+  return new Date(last_claim);
+};
+
+const setLastClaimTime = async (user) => {
+  const [
+    db,
+    user_id,
+  ] = await Promise.all([
+    getDB(),
+    getUserID(user),
+  ]);
+
+  const data = {
+    $user_id: user_id,
+    $date: new Date().toJSON(),
+  };
+
+  await db.run('UPDATE coins SET last_claim=$date WHERE user=$user_id', data);
+};
+
+module.exports = {
+  name        : 'daily',
+  aliases     : [],
+  description : 'Short description',
+  args        : [],
+  guildOnly   : true,
+  cooldown    : 3,
+  botperms    : ['SEND_MESSAGES'],
+  userperms   : ['SEND_MESSAGES'],
+  execute     : async (msg, args) => {
+    // Check if user claimed within the last 24 hours
+    const lastClaim = await getLastClaimTime(msg.author);
+    // TODO: display time left until next claim
+    if (lastClaim >= (Date.now() - DAY)) return msg.reply('You\'ve already claimed your coins for today..');
+
+    // Add the coins to the users balance, set last claimed time
+    const dailyAmount = 100;
+    // Add the balance then set last claim time (incase the user doesn't exist yet)
+    const balance = await addAmount(msg.author, dailyAmount, 'coins');
+    await setLastClaimTime(msg.author);
+    return msg.reply(`Claimed ${dailyAmount.toLocaleString('en-US')} <:money:737206931759824918>,\nCurrent Balance: ${balance.toLocaleString('en-US')} <:money:737206931759824918>`);
+  },
+};
