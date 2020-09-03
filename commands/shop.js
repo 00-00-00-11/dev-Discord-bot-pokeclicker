73d9b937e35490295216c1802934582553672bb7
RedSparr0w <admin@redsparr0w.com>
2020-09-03 21:33:16 +1200

    Able to purchase things from the shop now

--- commands/shop.js
+++ commands/shop.js
@@ -1,6 +1,12 @@
 const { MessageEmbed } = require('discord.js');
-const { getAmount } = require('../database.js');
+const { getAmount, removeAmount } = require('../database.js');
 const { shopItems, postPages } = require('../helpers.js');
+
+const generateCode = (discord_id, code_name) => {
+  code_name = `[Discord] ${code_name}`;
+  const val = discord_id ^ parseInt(code_name.replace(/(\W|_)/g, ''), 36);
+  return (val > 0 ? val : -val).toString(36).toUpperCase();
+};
 
 module.exports = {
   name        : 'shop',
@@ -10,7 +16,7 @@ module.exports = {
   guildOnly   : true,
   cooldown    : 3,
   botperms    : ['SEND_MESSAGES'],
-  userperms   : ['MANAGE_CHANNELS', 'MANAGE_MESSAGES'],
+  userperms   : ['SEND_MESSAGES'],
   execute     : async (msg, args) => {
     let [ page = 1 ] = args;
 
@@ -29,10 +35,83 @@ module.exports = {
         .addField('Price', `${item.price} <:money:737206931759824918>`, true)
         .addField('Description', item.description)
         .setFooter(`Balance: ${balance.toLocaleString('en-US')} | Page: ${index + 1}/${shopItems.length}`);
+        //.setFooter(``);
 
       pages.push({ embed });
     });
 
-    return postPages(msg, pages);
+    const botMsg = await postPages(msg, pages, page);
+    
+    await botMsg.react('737206931759824918');
+    const buyFilter = (reaction, user) => reaction.emoji.id === '737206931759824918' && user.id === msg.author.id;
+  
+    // Allow reactions for up to x ms
+    const timer = 1e5; // (100 seconds)
+    const buy = botMsg.createReactionCollector(buyFilter, {time: timer});
+
+    buy.on('collect', async r => {
+      botMsg.reactions.removeAll().catch(O_o=>{});
+      const itemID = (botMsg.embeds[0].footer.text.match(/(\d+)\//) || [])[1];
+
+      // Item doesn't exist or couldn't get item ID
+      if (!itemID || !shopItems[itemID - 1]) {
+        const embed = new MessageEmbed()
+          .setColor('#e74c3c')
+          .setDescription([
+            msg.author,
+            'Failed to purchase item',
+            'Something wen\'t wrong, try again later..',
+          ]);
+        return msg.channel.send({ embed });
+      }
+
+      const item = shopItems[itemID - 1];
+      const currentBalance = await getAmount(msg.author);
+
+      // Item too expensive
+      if (item.price > currentBalance) {
+        const embed = new MessageEmbed()
+          .setColor('#e74c3c')
+          .setDescription([
+            msg.author,
+            `**${item.name}** Failed to purhase!`,
+            '',
+            '_you cannot afford this item_',
+          ])
+          .setThumbnail(item.image)
+          .setFooter(`Balance: ${currentBalance.toLocaleString('en-US')}`);
+
+        return msg.channel.send({ embed });
+      }
+
+      // Purchase item
+      const remainingBalance = await removeAmount(msg.author, item.price);
+      const embed = new MessageEmbed()
+        .setColor('#2ecc71')
+        .setDescription([
+          msg.author,
+          `**${item.name}** Successfully purchased!`,
+          '',
+          '_code will be sent to you via direct message_',
+        ])
+        .setThumbnail(item.image)
+        .setFooter(`Balance: ${remainingBalance.toLocaleString('en-US')}`);
+
+      msg.channel.send({ embed });
+
+      const userEmbed = new MessageEmbed()
+        .setColor('#2ecc71')
+        .setDescription([
+          `**${item.name}** Successfully purchased!`,
+          '_Enter the following code in game to claim:_',
+          '```',
+          generateCode(msg.author.id, item.name),
+          '```',
+        ])
+        .setThumbnail(item.image)
+        .setFooter(`Balance: ${remainingBalance.toLocaleString('en-US')}`);
+
+      msg.author.send({ embed: userEmbed });
+    });
   },
 };
