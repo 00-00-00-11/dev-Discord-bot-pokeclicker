164dffb470ae7f5ebf86c002c000ffd67f9d0d02
RedSparr0w <admin@redsparr0w.com>
2020-12-10 23:33:46 +1300

    initial working

--- other/quiz.js
+++ other/quiz.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const { quizChannelID, website } = require('../config.js');
+const { getAmount, addAmount } = require('../database.js');
 const {
   pokemonList,
   randomFromArray,
@@ -19,38 +20,62 @@ const money_icon = '<:money:737206931759824918>';
 
 const newQuiz = async (guild) => {
   const quiz_channel = await guild.channels.cache.find(c => c.id == quizChannelID);
-  if (!quiz_channel) return console.log('no channel found');
+  if (!quiz_channel) return;
 
-  console.log('starting quiz');
   const quiz = randomFromArray(quizTypes)();
 
   const bot_message = await quiz_channel.send({ embed: quiz.embed });
 
   const filter = m => quiz.answer.test(m.content);
-  // errors: ['time'] treats ending because of the time limit as an error (1 minutes)
-  quiz_channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
-    .then(collected => {
+
+  // Time limit in minutes
+  const timeLimit = (25 + Math.floor(Math.random() * 5)) * 60 * 1000;
+
+  // errors: ['time'] treats ending because of the time limit as an error
+  quiz_channel.awaitMessages(filter, { max: 1, time:  timeLimit, errors: ['time'] })
+    .then(async collected => {
       const m = collected.first();
-      bot_message.delete().catch(e=>{});
-      m.delete().catch(e=>{});
-      m.reply(`\n+ ${quiz.amount} ${money_icon}`);
+
+      const balance = await addAmount(m.author, quiz.amount);
+
+      const embed = new MessageEmbed()
+        .setDescription([
+          `${m.author}`,
+          '**CORRECT!**',
+          `**+${quiz.amount} ${money_icon}**`,
+        ])
+        .setFooter(`Balance: ${balance.toLocaleString('en-US')}`)
+        .setColor('#2ecc71');
+      m.channel.send({ embed });
+
+      const botEmbed = bot_message.embeds[0];
+      botEmbed.setDescription(`${botEmbed.description.split('\n').map(l => `~~${l.trim()}~~`).join('\n')}`)
+        .setFooter(`Answered by ${m.member ? m.member.displayName : 'someone'}`)
+        .setColor('#e74c3c');
+      bot_message.edit({ embed: botEmbed });
     })
     .catch(collected => {
-      bot_message.edit('Better luck next time..');
+      const botEmbed = bot_message.embeds[0];
+      botEmbed.setDescription(`${botEmbed.description.split('\n').map(l => `~~${l.trim()}~~`).join('\n')}`)
+        .setFooter('Out of time!')
+        .setColor('#e74c3c');
+      bot_message.edit({ embed: botEmbed });
     });
+
+  setTimeout(() => newQuiz(guild), timeLimit);
 };
 
 const whosThatPokemon = () => {
   const pokemon = randomFromArray(pokemonList);
-  const answer = new RegExp(`^${pokemon.name}\\b`, 'i');
+  const answer = new RegExp(`^${pokemon.name.replace(/\s?\(.+/, '')}\\b`, 'i');
   const amount = Math.floor(Math.random() * 8) * 10 + 30;
 
   const shiny = !Math.floor(Math.random() * 128);
 
   const embed = new MessageEmbed()
     .setTitle('Who\'s that Pokemon?')
-    .setDescription(`Name this Pokemon to earn ${amount} ${money_icon}!`)
-    .setImage(`${website}assets/images/${shiny ? 'shiny' : ''}pokemon/${pokemon.id}.png`)
+    .setDescription(`Name the Pokemon!\n**+${amount} ${money_icon}**`)
+    .setThumbnail(`${website}assets/images/${shiny ? 'shiny' : ''}pokemon/${pokemon.id}.png`)
     .setColor('#3498db');
 
   return {
